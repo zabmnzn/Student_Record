10de6bec46cc0f4f96749016a78864723980c431
Zabdiel <zabdielmanzana@gmail.com>
2024-06-21 20:19:28 +0800

    Add project files.

---
+++ ADET-ACT2/AddWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static ADET_ACT2.MainWindow;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ADET_ACT2
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public Student NewStudent { get; set; }
+        private string connectionString = "SERVER=localhost;DATABASE=StudentDB;UID=root;PASSWORD=12345;";
+        public AddWindow(Student student = null)
+        {
+
+            InitializeComponent();
+
+
+            if (student != null)
+            {
+                IDbox.Text = student.ID;
+                FirstNbox.Text = student.FirstN;
+                MidNbox.Text = student.MiddleN;
+                LastNbox.Text = student.LastN;
+            }
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNumeric(IDbox.Text))
+            {
+                MessageBox.Show("ID should only contain numbers.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (FirstNbox.Text.Length > 100 || MidNbox.Text.Length > 100 || LastNbox.Text.Length > 100)
+            {
+                MessageBox.Show("First Name, Middle Name, and Last Name can only accept up to 100 characters.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (YearCbox.SelectedItem == null || SectionCbox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select level and section.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string insertQuery = "INSERT INTO studentdb.students (ID, FirstN, MiddleN, LastN, Year, Section) " + "VALUE (@ID, @FirstN, @MiddleN, @LastN, @Year, @Section)";
+                    MySqlCommand command = new MySqlCommand(insertQuery, connection);
+                    command.Parameters.AddWithValue("@ID", IDbox.Text);
+                    command.Parameters.AddWithValue("@FirstName", FirstNbox.Text);
+                    command.Parameters.AddWithValue("@MiddleName", MidNbox.Text);
+                    command.Parameters.AddWithValue("@LastName", LastNbox.Text);
+                    command.Parameters.AddWithValue("@YearLevel", YearCbox);
+                    command.Parameters.AddWithValue("@Section", SectionCbox.Text);
+
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error adding student record: {ex.Message}", "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            NewStudent = new Student
+            {
+                ID = IDbox.Text,
+                FirstN = FirstNbox.Text,
+                MiddleN = MidNbox.Text,
+                LastN = LastNbox.Text,
+                Year = (YearCbox.SelectedItem as ComboBoxItem)?.Content.ToString(),
+                Section = (SectionCbox.SelectedItem as ComboBoxItem)?.Content.ToString()
+            };
+            
+
+            this.DialogResult = true;
+            this.Close();
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+
+        }
+        private bool IsNumeric(string input)
+        {
+            return int.TryParse(input, out _);
+        }
+    }
+}
---
+++ ADET-ACT2/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADET_ACT2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Student> Students { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //initializing database connection
+            string connectionString = "SERVER=localhost;DATABASE=StudentDB;UID=root;PASSWORD=12345;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand("select * from studentdb.students", connection);
+            connection.Open();
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            connection.Close();
+            Students = new ObservableCollection<Student>();
+            dataGrid.DataContext = dt;
+            dataGrid.ItemsSource = Students;
+
+        }
+
+        public class Student
+        {
+            public string ID { get; set; }
+            public string FirstN { get; set; }
+            public string MiddleN { get; set; }
+            public string LastN { get; set; }
+            public string Year { get; set; }
+            public string Section { get; set; }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow AddWindow = new AddWindow();
+            if (AddWindow.ShowDialog() == true)
+            {
+                Students.Add(AddWindow.NewStudent);
+            }
+
+        }
+
+        private void btnUpd_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                Modify(dataGrid.SelectedItem as Student);
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to Modify.", "Update Student", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                Students.Remove(dataGrid.SelectedItem as Student);
+            }
+            else
+            {
+                MessageBox.Show("Please select a student to Delete.", "Delete Student", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void btnRef_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.Items.Refresh();
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void Modify(Student student)
+        {
+            AddWindow addWindow = new AddWindow(student);
+            if (addWindow.ShowDialog() == true)
+            {
+                if (student == null)
+                {
+
+                    Students.Add(addWindow.NewStudent);
+                }
+                else
+                {
+
+                    var existingStudent = Students.FirstOrDefault(s => s.ID == student.ID);
+                    if (existingStudent != null)
+                    {
+                        existingStudent.FirstN = addWindow.NewStudent.FirstN;
+                        existingStudent.MiddleN = addWindow.NewStudent.MiddleN;
+                        existingStudent.LastN = addWindow.NewStudent.LastN;
+                        existingStudent.Year = addWindow.NewStudent.Year;
+                        existingStudent.Section = addWindow.NewStudent.Section;
+                    }
+                }
+            }
+        }
+    }
+}
